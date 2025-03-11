6a16c7b8d1571918dcfd24e6f8584d36ae534ba9
Gabriel Villenave <gabriel@hyperproof.io>
2025-03-11 15:10:29 -0700

    Revert ESLint upgrade and minimum Node version

--- templates/oauth/rest-data-source/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import eslint from '@eslint/js';
-import prettierConfig from 'eslint-config-prettier';
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  prettierConfig
-);
